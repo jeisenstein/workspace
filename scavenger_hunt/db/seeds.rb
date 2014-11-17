48b9de58198931bb63fbab3710408967b118d46b
Jonathan Eisenstein <eisenste@unlv.nevada.edu>
2014-11-17 22:54:28 +0000

    Deleted Two Folders

--- scavenger_hunt/db/seeds.rb
+++ scavenger_hunt/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+2.times do |e|
+    event = Event.create(name:"Event #{e + 1}")
+
+
+    3.times do |l|
+        qr= ((0..9).to_a + ('a'..'z').to_a + ('A'..'Z').to_a).shuffle[0..7].join
+        location = Location.create(event_id: event.id,
+        name: "location #{l + 1} belongs to #{event.id}",
+        id: qr)
+    end
+    
+end
