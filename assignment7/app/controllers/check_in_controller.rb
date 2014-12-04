6907d646d0fdadb71bf8b75b3935b4428e9fbd2d
Jonathan Eisenstein <eisenste@unlv.nevada.edu>
2014-12-04 09:08:44 +0000

    Finishing Touches on QRCODE App

--- assignment7/app/controllers/check_in_controller.rb
+++ assignment7/app/controllers/check_in_controller.rb
@@ -5,17 +5,25 @@ class CheckInController < ApplicationController
 
   def success
     @participant = Participant.find_by(email: params[:email])
+    if @participant.nil? == false then
     @participant.visited_locations << params[:location_id].to_i
     @participant.save
-
+  end
     respond_to do |format|
-      if @participant.save
+       if ! @participant.nil?
         format.html { redirect_to participant_path(@participant), notice: 'Participant was successfully checked in.' }
         format.json { render :show, status: :created, location: @participant }
       else
-        format.html { render :new }
-        format.json { render json: @participant.errors, status: :unprocessable_entity }
+        @participant = Participant.create(:email => params[:email], :event_id => params[:event_id], :visited_locations => [params[:location_id].to_i])
+        @participant.visited_locations << params[:location_id].to_i
       end
+        if @participant.save
+        Location.where(event_id: @participant.event_id).each do |l|
+          l.participants << @participant
+        end
+        format.html { redirect_to participant_path(@participant), notice: 'Participant was successfully checked in.' }
+        format.json { render :show, status: :created, location: @participant }
+    end
     end
   end
 end
